8a442c4892cb8d2602853c699ad390f4f74f85a9
Võ Thành Thông <61813168+vothanhthong@users.noreply.github.com>
2023-09-15 20:51:37 +0700

    Test commit and push setting view

---
+++ MuscleDiscovery/EditProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  EditProfileView.swift
+//  MuscleDiscovery
+//
+//  Created by Vo Thanh Thong on 15/09/2023.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProfileView()
+    }
+}
